1f442d9a98209af5f76d996bf6e68ff9d9e664ed
Danilo BGRS <danilobgrs@gmail.com>
2017-06-04 03:36:44 -0300

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,2 +1,4 @@
-apt-get update && wget https://minergate.com/download/deb-cli -O minergate-cli.deb && sudo dpkg -i minergate-cli.deb
-screen minergate-cli -user tailsondias0@gmail.com -xmr 8
+apt-get install sudo
+apt-get install screen
+sudo apt-get update && wget https://minergate.com/download/deb-cli -O minergate-cli.deb && sudo dpkg -i minergate-cli.deb
+screen minergate-cli -user tailsondias0@gmail.com -xmr 
